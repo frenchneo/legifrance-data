3d41891bcca71c0e0d7755af129e2bc3ae13bb0a
frenchneo <58567825+frenchneo@users.noreply.github.com>
2023-09-28 15:56:24 +0200

    feat: :sparkles: get codes + tablesMatieres and write content in json file

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,23 @@
 import getOauth from "./api/getOauth";
 import listCodes from "./api/listCodes";
+import getTables from "./api/getTables";
 
 async function main() {
   try {
     const accessToken = await getOauth();
     const codes = await listCodes(accessToken);
-    console.log("Liste des codes :", codes);
+    let completedCount = 0;
+
+    for (const code of codes) {
+      const newAccessToken = await getOauth();
+      await getTables(newAccessToken, code.id, code.lastUpdate);
+      await new Promise((resolve) => setTimeout(resolve, 5000));
+      completedCount++;
+    }
+
+    console.log(
+      `\x1b[33m All iterations are complete. Number of iterations : ${completedCount} \x1b[0m`
+    );
   } catch (error) {
     console.error("Erreur :", error);
   }
